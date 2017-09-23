f599397bdae05ded62697b1bc741f829e93206c4
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-09-23 10:37:11 +0200

    Welcome Amazons Price Category

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@
     'use strict';
 
     $(document).ready(function () {
-        var finalDate = moment('2017-09-30T19:00:00+02:00');
+        var finalDate = moment('2017-09-30T09:30:00+02:00');
         var now = moment().unix();
 
         $('div#clock-wrapper').FlipClock(finalDate.unix() - now, {
